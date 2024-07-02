dca308bf0e8b9cdf3d56e8fb8102dc4cc6b5391b
Panisa <panisanat19@gmail.com>
2024-07-02 18:12:35 +0700

    Finished

--- q2.js
+++ q2.js
@@ -1,18 +1,15 @@
 function arrayStringify(a) {
-  let result = '';
-    for (let num of a) {
+    var result = '';
+    for (var _i = 0, a_1 = a; _i < a_1.length; _i++) {
+        var num = a_1[_i];
         result += num;
     }
     return result;
-
 }
-
-const c1 = [1, 2, 3];
-const c2 = [10, 9, 8, 7, 6, 5];
-const c3 = [];
-
+var c1 = [1, 2, 3];
+var c2 = [10, 9, 8, 7, 6, 5];
+var c3 = [];
 console.log(arrayStringify(c1));
 console.log(arrayStringify(c2));
 console.log(arrayStringify(c3));
-
 module.exports = arrayStringify;
--- q3.js
+++ q3.js
@@ -1,21 +1,20 @@
 function primeNumber(a) {
-  let results = [];
-    for (let num of a) {
+    var results = [];
+    for (var _i = 0, a_1 = a; _i < a_1.length; _i++) {
+        var num = a_1[_i];
         if (num >= 10) {
             results.push("YES");
-        } else {
+        }
+        else {
             results.push("NO");
         }
     }
     return results;
 }
-
-const d1 = 10;
-const d2 = 29;
-const d3 = 2;
-
+var d1 = 10;
+var d2 = 29;
+var d3 = 2;
 console.log(primeNumber(d1));
 console.log(primeNumber(d2));
 console.log(primeNumber(d3));
-
 module.exports = primeNumber;
